a091d7f3e66814c6560089d07091b36cbf2ce49e
Aqib-dev05 <aqibgithub@gmail.com>
2025-06-01 16:01:04 +0500

    Images are shown upon weather updates

--- src/Show.jsx
+++ src/Show.jsx
@@ -1,9 +1,39 @@
-
 import { Wind, Waves } from "lucide-react";
+import Sunny from "./assets/sunny.png";
+import FewClounds from "./assets/fewClouds.png";
+import Scatter from "./assets/cloudy.jpeg";
+import Broken from "./assets/brokenClouds.png";
+import Over from "./assets/overcastClouds.png";
+import Rain from "./assets/rain.png";
+import Snow from "./assets/snow.png";
+import Haze from "./assets/haze.png";
+import Thunder from "./assets/thunderstorm.png";
+import Smoke from "./assets/smoke.png";
+import Dust from "./assets/dust.png";
+
+
+
 export default function Show({ data }) {
+    let cleanDesc=data.description.replace(/\s+/g,'');
+    const images = {
+    clearsky:Sunny,
+    fewclouds: FewClounds,
+    scatteredclouds:Scatter,
+    brokenclouds: Broken,
+    overcastclouds: Over,
+    lightrain: Rain,
+    moderaterain: Rain,
+    heavyintensityrain:Rain, 
+    snow: Snow,
+    haze: Haze,
+    thunderstorm:Thunder, 
+    smoke: Smoke,
+    dust: Dust
+}
+
     return (
         <div className="w-full  flex flex-col  items-center gap-5">
-            {/* <img className="size-[200px] object-cover " alt="pic" /> */}
+            <img src={images[cleanDesc]} className="size-[200px] object-cover " alt="pic" />
             <div className="flex flex-col justify-center items-center" >
                 <h1 className="text-[66px] font-bold " >{data.temp}<span className="pl-1 text-[26px] font-medium ">&deg;C</span> </h1>
                 <p className="text-[25px] font-semibold" >{data.description}</p>
@@ -24,3 +54,4 @@ export default function Show({ data }) {
         </div>
     )
 }
+
