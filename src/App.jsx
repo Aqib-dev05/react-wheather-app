c511420a2c8bf95449c160a9ddbb79d4b8d3c01f
Aqib-dev05 <aqibgithub@gmail.com>
2025-06-01 11:33:04 +0500

    All STylings are done.Responsiveness checked.

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ function App() {
 
   return (
     <>
-      <div className="h-[100vh] w-[100vw] flex justify-center items-center bg-gradient-to-r from-blue-500 to-purple-500">
-        <div className="bg-white max-md:w-[100%] w-[600px]  rounded-lg shadow-lg flex flex-col  items-center gap-5">
-          <form className=" w-full h-[100px] relative p-4 flex justify-center items-center gap-6" >
+      <div className="h-[100vh]  w-[100vw] flex justify-center items-center bg-gradient-to-r from-blue-500 to-purple-500">
+        <div className="bg-white pb-10 max-md:w-[100%] w-[500px] rounded-4xl shadow-lg flex flex-col  items-center gap-5">
+          <form className=" w-full h-[100px]  relative p-4 flex justify-center items-center gap-6" >
             <label htmlFor="inp" className="absolute max-sm:left-1  left-7" ><MapPin /></label>
             <input type="text" id="inp" placeholder='Enter City Name' className='pl-7 border-b-2 border-black w-[80%] py-2 focus:outline-0 text-[22px] uppercase ' />
             <button type='submit' className='w-[55px] h-[55px] rounded-[50%]  bg-blue-100 hover:bg-black hover:text-white transition duration-150 flex justify-center items-center ' ><Search size={27} strokeWidth={3} /></button>
           </form>
-          <Show/>
+          <Show />
         </div>
       </div>
 
--- src/Show.jsx
+++ src/Show.jsx
@@ -1,9 +1,26 @@
+import Cloud from "./assets/cloudy.jpeg"
+import { Wind, Waves } from "lucide-react";
 export default function Show() {
+    return (
+        <div className="w-full  flex flex-col  items-center gap-5">
+            <img src={Cloud} className="size-[200px] object-cover " alt="pic" />
+            <div className="flex flex-col justify-center items-center" >
+                <h1 className="text-[66px] font-bold " >34<span className="pl-1 text-[26px] font-medium ">&deg;C</span> </h1>
+                <p className="text-[25px] font-semibold" >Clear Sky</p>
+            </div>
+            <div className=" mt-3 w-full flex items-center justify-center gap-8 max-sm:gap-2">
+                <div className="flex items-center justify-center gap-1" >
+                    <div> <Waves size={40} strokeWidth={3} /></div>
+                    <div className="flex flex-col justify-center text-[25px] max-sm:text-[20px] font-bold " > <p>28%</p> <p className="font-medium " >Humidity</p> </div>
+                </div>
 
-    return(
+                <div className="flex items-center justify-center" >
+                    <div> <Wind size={40} strokeWidth={3} /></div>
+                    <div className="flex flex-col justify-center text-[25px] max-sm:text-[20px] font-bold " > <p>2 km/h</p> <p className="font-medium " >Wind Speed</p> </div>
+                </div>
 
-        <>
-        <h1>Hello</h1>
-        </>
+            </div>
+
+        </div>
     )
 }
