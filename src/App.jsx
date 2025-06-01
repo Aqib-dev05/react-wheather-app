9c6414b1a30820618f35869dd59da5e7e151fd80
Aqib-dev05 <aqibgithub@gmail.com>
2025-06-01 10:09:36 +0500

    STylings are made

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-
-import './App.css'
+import { Search, MapPin } from "lucide-react";
+import './App.css';
+import Show from "./Show";
 
 function App() {
 
 
   return (
     <>
-    <div className="container">
-        <div className="inner">
-          <form >
-            <input type="text" />
-            <button type='submit'>get</button>
+      <div className="h-[100vh] w-[100vw] flex justify-center items-center bg-gradient-to-r from-blue-500 to-purple-500">
+        <div className="bg-white max-md:w-[100%] w-[600px]  rounded-lg shadow-lg flex flex-col  items-center gap-5">
+          <form className=" w-full h-[100px] relative p-4 flex justify-center items-center gap-6" >
+            <label htmlFor="inp" className="absolute max-sm:left-1  left-7" ><MapPin /></label>
+            <input type="text" id="inp" placeholder='Enter City Name' className='pl-7 border-b-2 border-black w-[80%] py-2 focus:outline-0 text-[22px] uppercase ' />
+            <button type='submit' className='w-[55px] h-[55px] rounded-[50%]  bg-blue-100 hover:bg-black hover:text-white transition duration-150 flex justify-center items-center ' ><Search size={27} strokeWidth={3} /></button>
           </form>
-          <div className="show"></div>
+          <Show/>
         </div>
-    </div>
+      </div>
 
     </>
   )
@@ -22,4 +24,4 @@ function App() {
 
 export default App
 //  const KEY = "814b2831296e18e91b491c10618ddef7";
-    // const URL = "https://api.openweathermap.org/data/2.5/weather?q={city}&appid={KEY}";
+// const URL = "https://api.openweathermap.org/data/2.5/weather?q={city}&appid={KEY}";
---
+++ src/Show.jsx
@@ -0,0 +1,9 @@
+export default function Show() {
+
+    return(
+
+        <>
+        <h1>Hello</h1>
+        </>
+    )
+}
